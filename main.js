903f430662f10a3048e2e87913caa3c684a20392
ToniBiro <33375662+ToniBiro@users.noreply.github.com>
2019-03-09 20:06:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,48 +1,121 @@
-class TreeNode{
-    constructor(info){
+///assert
+
+function assert(bool, mess) {
+    if (bool === false)
+        throw Error(mess);
+}
+
+///tests
+
+function testNodeParent() {
+    let father = new TreeNode;
+    let son = new TreeNode;
+    father.addChild(son);
+    assert(father.children[0].parent === father, "Son's parent is not father!");
+}
+
+function testEmptiestChild() {
+    let wagon = new TreeNode;
+    let c1 = new Compartment(4);
+    let c2 = new Compartment(3);
+    let c3 = new Compartment(1);
+    let c4 = new Compartment(0);
+
+    wagon.addChild(c1);
+    wagon.addChild(c2);
+    wagon.addChild(c3);
+    wagon.addChild(c4);
+
+    assert(wagon.getEmptiestChild() === c4, "Wrong EmptiestChild chosen!");
+    ///assert(wagon.getEmptiestChild() === null, "Emptiest child is empty wagon!");
+}
+
+///function declarations
+
+///classes
+
+class TreeNode {
+    constructor(fs) {
         this.parent = null;
         this.children = [];
+        this.freeSeats = fs;
     }
-
-    addChild(child){
+    ///adds a new child (node) to this node, the parameter is the child
+    addChild(child) {
         this.children.push(child);
+        child.parent = this;
+    }
+    getEmptiestChild(){
+        let min = this.children[0].freeSeats;
+        let child = this.children[0];
+        let lg = this.children.length;
+        for(let i = 1; i < lg; ++i){
+            if(this.children[i].freeSeats < min)
+            {
+                min = this.children[i].freeSeats;
+                child = this.children[i];
+            }
+        }
+        return child;
     }
 }
 
 class Compartment {
-    constructor(){
+    constructor() {
         this.parent = null;
-        this.freeSeats = 0;
+        this._freeSeats = 0;
     }
-    get nrSeats(){
+    get freeSeats(){
+        return this._freeSeats;
+    }
+    set freeSeats(value) {
+        this._freeSeats += value;
+        if (this._freeSeats < 0 || this._freeSeats > 8)
+            throw Error("Wrong alocation of seats in compartment!");
+    }
+    
+
+    get nrSeats() {
         return this.freeSeats;
     }
 }
 
-class Ticket{
-    constructor(start, finish){
+class Ticket {
+    constructor(start, finish) {
         this.Compartment = undefined;
         this.start = start;
         this.finish = finish;
     }
 }
 
-class Train{
-    constructor(){
-        let mainNode = new TreeNode;
+class Train {
+    constructor() {
+        let root = new TreeNode;
+
+        for (let i = 0; i < 5; ++i) {
+            let wagon = new TreeNode;
+            for (let j = 0; j < 10; ++j) {
+                let comp = new Compartment;
+                wagon.addChild(comp);
+            }
+            root.addChild(wagon);
+        }
+        this.root = root;
 
     }
 }
 
-let root = new TreeNode();
+///other stuff
 
-for(let i = 0; i < 5; ++i)
-{
-    let wagon = new TreeNode;
-    for(let j = 0; j < 10; ++j)
-    {
-        let comp = new Compartment;
-        wagon.addChild(comp);
-    }
-    root.addChild(wagon);
+let r = new TreeNode;
+let wag = new TreeNode;
+
+r.addChild(wag);
+if (r.children[0].parent === r)
+    console.log("merge");
+else {
+    console.log("nu merge");
 }
+
+testNodeParent();
+testEmptiestChild();
