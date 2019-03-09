fd7cb96de3cc31b1b65910f8155ace57057338d1
Gabriel Majeri <gabriel.majeri6@gmail.com>
2019-03-09 18:27:15 +0200

    Add basic UI for tickets

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+const STATIONS = [
+    "Brașov", "Predeal", "Azuga", "Bușteni",
+    "Sinaia", "Câmpina", "Ploiești Vest", "București Nord",
+];
+
+const WAGON_COUNT = 5;
+const COMPARTMENTS_PER_WAGON = 10;
+const COMPARTMENT_CAPACITY = 8;
+
 console.log("Hello world!")
---
+++ ui.js
@@ -0,0 +1,18 @@
+const train = document.getElementById("train");
+for (let i = 0; i < WAGON_COUNT; ++i) {
+    const wagon = document.createElement("div");
+    wagon.className = "wagon";
+
+    const wagonNumber = document.createElement("span");
+    wagonNumber.textContent = "Vagon " + (i + 1) + ":";
+    wagon.appendChild(wagonNumber);
+
+    for (let j = 0; j < COMPARTMENTS_PER_WAGON; ++j) {
+        const compartment = document.createElement("span");
+        compartment.className = "compartment";
+        compartment.textContent = "0/" + COMPARTMENT_CAPACITY;
+        wagon.appendChild(compartment);
+    }
+
+    train.appendChild(wagon);
+}
