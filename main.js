18e505a094861b02ce92dd4b8531682b98f8c808
ToniBiro <33375662+ToniBiro@users.noreply.github.com>
2019-03-09 16:31:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-console.log("Hello world!")
+console.log("inceput");
+class TreeNode{
+    constructor(info){
+        this.info = info;
+        this.children = [];
+    }
+    get data(){
+        return this.info;
+    }
+
+}
+
+class Compartment {
+    constructor(){
+        this.freeSeats = 0;
+    }
+    get nrSeats(){
+        return this.freeSeats;
+    }
+}
+
+let root = new TreeNode();
+
+for(let i = 0; i < 5; ++i)
+{
+    let wagon = new TreeNode;
+    for(let j = 0; j < 10; ++j)
+    {
+        let comp = new Compartment;
+        wagon.children.push(comp);
+    }
+    root.children.push(wagon);
+}
+
+root.children[0].children[0] = 71;
+console.log(root.children[0].children[0]);
