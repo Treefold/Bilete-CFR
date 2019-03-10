cb7cee43dc1ca65c1e5b54a1b708c5384d47389c
Gabriel Majeri <gabriel.majeri6@gmail.com>
2019-03-10 22:06:43 +0200

    Update UI

--- ui.js
+++ ui.js
@@ -1,53 +1,39 @@
+function getStationName(idx) {
+    const stationName = STATIONS[idx];
+    return stationName === undefined ? (idx + 1).toString() : stationName;
+}
+
 // Dep + Arr Select Option
 (function () {
     const dep_st = document.getElementById("departure-station");
     const arr_st = document.getElementById("arrival-station");
     let option = document.createElement('option');
     option.value = 0;
-    option.textContent = (STATIONS[0] === undefined) ? 1 : STATIONS[0];
+    option.textContent = getStationName(0);
     //option.onclick = "dep_arr();";
     dep_st.appendChild(option);
     for (let i = 1; i < STATIONS_NUMBER - 1; ++i) {
         option = document.createElement('option');
         option.value = i;
-        option.textContent = (STATIONS[i] === undefined) ? i + 1 : STATIONS[i];
+        option.textContent = getStationName(i);
         //option.onclick = "dep_arr();";
         dep_st.appendChild(option);
         option = document.createElement('option');
         option.value = i;
-        option.textContent = (STATIONS[i] === undefined) ? i + 1 : STATIONS[i];
+        option.textContent = getStationName(i);
         arr_st.appendChild(option);
     }
     option = document.createElement('option');
     option.value = STATIONS_NUMBER - 1;
-    option.textContent = (STATIONS[STATIONS_NUMBER - 1] === undefined) ? STATIONS_NUMBER : STATIONS[STATIONS_NUMBER - 1];
+    option.textContent = getStationName(STATIONS_NUMBER - 1);
     arr_st.appendChild(option);
-})();
 
-// Cancel Ticket Select Option
-(function () {
-    const c_wagon = document.getElementById("cancel-wagon");
-    for (let i = 0; i < WAGON_COUNT; ++i) {
-        const option = document.createElement('option');
+    const routeSectionEl = document.getElementById("route-section");
+    for (let i = 0; i < STATIONS_NUMBER; ++i) {
+        option = document.createElement('option');
         option.value = i;
-        option.textContent = i + 1;
-        c_wagon.appendChild(option);
-    }
-
-    const c_compartment = document.getElementById("cancel-compartment");
-    for (let i = 0; i < COMPARTMENTS_PER_WAGON; ++i) {
-        const option = document.createElement('option');
-        option.value = i;
-        option.textContent = i + 1;
-        c_compartment.appendChild(option);
-    }
-
-    const c_seat = document.getElementById("cancel-seat");
-    for (let i = 0; i < COMPARTMENT_CAPACITY; ++i) {
-        const option = document.createElement('option');
-        option.value = i;
-        option.textContent = i + 1;
-        c_seat.appendChild(option);
+        option.textContent = getStationName(i) + " - " + getStationName(i + 1);
+        routeSectionEl.appendChild(option);
     }
 })();
 
@@ -66,17 +52,15 @@
         wagonEl.appendChild(wagonNumberEl);
 
         for (let j = 0; j < COMPARTMENTS_PER_WAGON; ++j) {
-            const compartment = wagon.compartments[j];
             const compartmentEl = document.createElement('span');
             compartmentEl.className = "compartment";
-
-            compartmentEl.textContent = formatCompartmentOccupancy(compartment);
-            compartmentEl.style.background = formatCompartmentGradient(compartment);
 
             wagonEl.appendChild(compartmentEl);
         }
         trainEl.appendChild(wagonEl);
     }
+
+    updateTrainDisplay();
 })(CFR);
 
 function addTicket() {
@@ -90,13 +74,35 @@ function addTicket() {
         return;
     }
 
-    // Change train display
+    updateTrainDisplay();
+}
+
+function formatCompartmentOccupancy(compartment, section) {
+    const occupiedSeats = COMPARTMENT_CAPACITY - compartment.freeSeats[section];
+    return occupiedSeats + "/" + COMPARTMENT_CAPACITY;
+}
+
+function formatCompartmentGradient(compartment, section) {
+    const percentFree = compartment.freeSeats[section] / COMPARTMENT_CAPACITY;
+    const percentFull = 1 - percentFree;
+
+    return "linear-gradient(to right, lightgray " + (percentFull * 100) +
+        "%, white " + (percentFull * 100) + "%)";
+}
+
+// Changes the train display to reflect the in-memory data
+function updateTrainDisplay() {
+    const routeSectionEl = document.getElementById("route-section");
+    const section = parseInt(routeSectionEl.value);
+
     let wagons = document.getElementById("train").childNodes;
     for (let i = 0; i < WAGON_COUNT; ++i) {
         let compartments = wagons[i].getElementsByClassName("compartment");
         for (let j = 0; j < COMPARTMENTS_PER_WAGON; ++j) {
-            compartments[j].textContent = formatCompartmentOccupancy(CFR.wagons[i].compartments[j]);
-            compartments[j].style.background = formatCompartmentGradient(CFR.wagons[i].compartments[j]);
+            let wagon = CFR.wagons[i];
+            let compartment = wagon.compartments[j];
+            compartments[j].textContent = formatCompartmentOccupancy(compartment, section);
+            compartments[j].style.background = formatCompartmentGradient(compartment, section);
         }
     }
 }
@@ -107,7 +113,7 @@ function addAllTickets() {
     document.getElementById("tickets").value.split('\n').forEach(function (element) {
         let now = element.trim().split(/\s+/);
         groupSize = parseInt(now[0]);
-        start = parseInt(now[1]);
+        start = parseInt(now[1]) - 1;
         duration = parseInt(now[2]);
         if (isNaN(groupSize) || groupSize < 1 || groupSize > GROUP_MAX_SIZE) { return; }
         if (isNaN(start) || start < 0 || start >= STATIONS_NUMBER - 1) { return; }
@@ -130,13 +136,5 @@ function dep_arr() {
         arr_st[i].style.display = (i < dep_station) ? "none" : "block";
     }
 
-    // Change train display
-    let wagons = document.getElementById("train").childNodes;
-    for (let i = 0; i < WAGON_COUNT; ++i) {
-        let compartments = wagons[i].getElementsByClassName("compartment");
-        for (let j = 0; j < COMPARTMENTS_PER_WAGON; ++j) {
-            compartments[j].textContent = formatCompartmentOccupancy(CFR.wagons[i].compartments[j]);
-            compartments[j].style.background = formatCompartmentGradient(CFR.wagons[i].compartments[j]);
-        }
-    }
+    updateTrainDisplay();
 }
