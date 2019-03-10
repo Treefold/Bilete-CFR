01a7362f117154367d911a11e1530e2c2a028f6a
Gabriel Majeri <gabriel.majeri6@gmail.com>
2019-03-10 09:35:10 +0200

    Update and rename main.js to main.html # Conflicts: #	main.js

--- ui.js
+++ ui.js
@@ -1,26 +1,53 @@
-const train = document.getElementById("train");
-let CFR = new Train
-
-for (let i = 0; i < WAGON_COUNT; ++i) {
-    const wagon = document.createElement('div');
-    wagon.className = "wagon";
-
-    const wagonNumber = document.createElement('span');
-    wagonNumber.textContent = "Vagon " + (i + 1) + ":";
-    wagon.appendChild(wagonNumber);
-
-    for (let j = 0; j < COMPARTMENTS_PER_WAGON; ++j) {
-        const compartment = document.createElement('span');
-        compartment.className = "compartment";
-        compartment.textContent = "0/" + COMPARTMENT_CAPACITY;
-        wagon.appendChild(compartment);
+(function(){
+    const dep_st = document.getElementById("departure-station");
+    const arr_st = document.getElementById("arrival-station");
+    let option = document.createElement('option');
+    option.value = 0;
+    option.innerHTML = STATIONS[0];
+    dep_st.appendChild(option);
+    for (let i = 1; i < STATIONS_NUMBER-1; ++i) {
+        option = document.createElement('option');
+        option.value = i;
+        option.innerHTML = STATIONS[i];
+        dep_st.appendChild(option);
+        option = document.createElement('option');
+        option.value = i;
+        option.innerHTML = STATIONS[i];
+        arr_st.appendChild(option);
     }
+    option = document.createElement('option');
+    option.value = STATIONS_NUMBER-1;
+    option.innerHTML = STATIONS[STATIONS_NUMBER-1];
+    arr_st.appendChild(option);
+})();
 
-    train.appendChild(wagon);
-}
 
 (function(){
-    const c_wagon = document.getElementById("cancelled-wagon");
+  const train = document.getElementById("train");
+  let CFR = new Train
+
+  for (let i = 0; i < WAGON_COUNT; ++i) {
+      const wagon = document.createElement('div');
+      wagon.className = "wagon";
+
+      const wagonNumber = document.createElement('span');
+      wagonNumber.textContent = "Vagon " + (i + 1) + ":";
+      wagon.appendChild(wagonNumber);
+
+      for (let j = 0; j < COMPARTMENTS_PER_WAGON; ++j) {
+          const compartment = document.createElement('span');
+          compartment.className = "compartment";
+          compartment.textContent = "0/" + COMPARTMENT_CAPACITY;
+          wagon.appendChild(compartment);
+      }
+
+      train.appendChild(wagon);
+  }
+})();
+
+
+(function(){
+    const c_wagon = document.getElementById("cancel-wagon");
     for (let i = 0; i < WAGON_COUNT; ++i) {
         const option = document.createElement('option');
         option.value = i;
@@ -28,15 +55,15 @@ for (let i = 0; i < WAGON_COUNT; ++i) {
         c_wagon.appendChild(option);
     }
 
-    const c_compartment = document.getElementById("cancelled-compartment");
+    const c_compartment = document.getElementById("cancel-compartment");
     for (let i = 0; i < COMPARTMENTS_PER_WAGON; ++i) {
         const option = document.createElement('option');
         option.value = i;
         option.innerHTML = i + 1;
         c_compartment.appendChild(option);
     }
-  
-    const c_seat = document.getElementById("cancelled-seat");
+
+    const c_seat = document.getElementById("cancel-seat");
     for (let i = 0; i < COMPARTMENT_CAPACITY; ++i) {
         const option = document.createElement('option');
         option.value = i;
