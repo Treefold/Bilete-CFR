9db1870cc2fb1ac108856ab76d89a981ed6f13c7
Gabriel Majeri <gabriel.majeri6@gmail.com>
2019-03-10 09:28:01 +0200

    Update main.js # Conflicts: #	main.js

--- main.js
+++ main.js
@@ -110,24 +110,3 @@ class Train {
         this.root = root;
     }
 }
-
-const train = document.getElementById("train");
-let CFR = new Train
-
-for (let i = 0; i < WAGON_COUNT; ++i) {
-    const wagon = document.createElement("div");
-    wagon.className = "wagon";
-
-    const wagonNumber = document.createElement('span');
-    wagonNumber.textContent = "Vagon " + (i + 1) + ":";
-    wagon.appendChild(wagonNumber);
-
-    for (let j = 0; j < COMPARTMENTS_PER_WAGON; ++j) {
-        const compartment = document.createElement('span');
-        compartment.className = "compartment";
-        compartment.textContent = "0/" + COMPARTMENT_CAPACITY;
-        wagon.appendChild(compartment);
-    }
-
-    train.appendChild(wagon);
-}
